e5c8618c1f94b53598339e2f52e2584de2cc2b20
JvanMeurs <jeroenvanmeurs@gmail.com>
2024-12-05 20:41:49 +0100

    finished day 3

--- day03/day03.py
+++ day03/day03.py
@@ -1,24 +1,29 @@
 import re
 
-data = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-
 def find_mul(data):
     return re.findall(r"mul\(\d+,\d+\)",data)
 
 def find_digit(data):
     return re.findall(r"\d+",data)
 
+def filter_dont(data):
+    return (re.split(r"don't\(\)",j)[0] for j in re.split(r"do\(\)",data))
 
 def multiply_and_sum(data):
     return sum(int(i) * int(j) for [i,j] in (find_digit(mul) for mul in find_mul(data)))
+
+def filter_intructions(data):
+    return sum(multiply_and_sum(i) for i in filter_dont(data))
 
 if __name__ == ("__main__"):
     with open("sample_input.txt",'r') as input_file:
         data = input_file.read()
 
     print(multiply_and_sum(data))
+    print(filter_intructions(data))
 
     with open("input.txt",'r') as input_file:
         data = input_file.read()
 
     print(multiply_and_sum(data))
+    print(filter_intructions(data))
