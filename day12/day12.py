f472935511d43dc7f791568bfdd92b913834f8b8
JvanMeurs <jeroenvanmeurs@gmail.com>
2024-12-16 22:50:23 +0100

    day 12 part 2. a bit ugly

--- day12/day12.py
+++ day12/day12.py
@@ -9,13 +9,25 @@ def traverse_grid(grid):
     nr_cols = len(grid[0])
 
     visited_global = [[False] * nr_cols for _ in range(nr_rows)]
-
+    
     adjacency = [(1,0), (-1,0), (0,1), (0,-1)]
-    def dfs_rec(visited, curr_row, curr_col):
+    relative_vertices = [(0,0), (1,0), (0,1), (1,1)]
+
+    def fill_vertices(vertices, curr_row,curr_col):
+        for delta_vrow, delta_vcol in relative_vertices:
+            key = (curr_row+delta_vrow,curr_col+delta_vcol)
+            if key in vertices:
+                vertices[key] += 1
+            else:
+                vertices[key] = 1
+        return vertices
+    
+    def dfs_rec(visited, curr_row, curr_col,vertices):
         
+        vertices = fill_vertices(vertices, curr_row,curr_col)
+
         area = 1
-        perimiter = 0
-        sides = 4
+        perimeter = 0
         for delta_row, delta_col in adjacency:
             new_row, new_col = curr_row + delta_row, curr_col + delta_col
             if 0 <= new_row < nr_rows and 0 <= new_col < nr_cols:
@@ -23,38 +35,55 @@ def traverse_grid(grid):
                     if grid[new_row][new_col] == grid[curr_row][curr_col]:
                         visited[new_row][new_col] = True
                         visited_global[new_row][new_col] = True
-                        a,p = dfs_rec(visited, new_row, new_col)
+                        a,p,vertices  = dfs_rec(visited, new_row, new_col,vertices)
                         area += a
-                        perimiter += p
+                        perimeter += p
                     else:
-                        perimiter += 1
+                        perimeter += 1
             else:
-                perimiter += 1
+                perimeter += 1
 
-        return area,perimiter
+        return area,perimeter,vertices
                 
     def dfs(start_row, start_col):
         visited = [[False] * nr_cols for _ in range(nr_rows)]
         visited[start_row][start_col] = True
+        vertices = {}
         visited_global[start_row][start_col] = True
-        return dfs_rec(visited, start_row, start_col)
+        return dfs_rec(visited, start_row, start_col, vertices),visited
 
     plants = {}
     for r in range(nr_rows):
         for c in range(nr_cols):
             if not visited_global[r][c]:
-                total_area,total_perimiter = dfs(r,c)
-                plants[len(plants)]={"area": total_area, "perimiter":total_perimiter}
+                (total_area,total_perimeter,vertices),visited = dfs(r,c)
+                sides = 0
+                for vertex,count in vertices.items():
+                    sides += count%2
+                    if count == 2:
+                        if 0 <= vertex[0] - 1 < nr_rows and 0 <= vertex[1] - 1 < nr_cols and 0 <= vertex[0] < nr_rows and 0 <= vertex[1] < nr_cols:
+                            if not visited[vertex[0] - 1][vertex[1] - 1] and not visited[vertex[0]][vertex[1]]:
+                                sides += 2
+                        if 0 <= vertex[0] - 1 < nr_rows and 0 <= vertex[1] < nr_cols and 0 <= vertex[0] < nr_rows and 0 <= vertex[1] - 1 < nr_cols:
+                            if not visited[vertex[0] - 1][vertex[1]] and not visited[vertex[0]][vertex[1] - 1]:
+                                sides += 2
+                plants[len(plants)]={"area": total_area, "perimeter":total_perimeter,"sides": sides}
 
     return plants
 
 def calculate_total_cost(plants):
-    return sum(plants[key]["area"]*plants[key]["perimiter"] for key in plants.keys())
+    costs = 0
+    bulk_costs = 0
+    for key in plants.keys():
+        costs += plants[key]["area"]*plants[key]["perimeter"]
+        bulk_costs += plants[key]["area"]*plants[key]["sides"]
+    return costs,bulk_costs
 
 if __name__ == ("__main__"): # pragma: no cover
     file_name = "input"
     data = read_input(file_name)
     plants = traverse_grid(data)
-    costs = calculate_total_cost(plants)
+    costs, bulk_costs = calculate_total_cost(plants)
     print(f"Answer to day 12 part 1: {costs}")
+    print(f"Answer to day 12 part 2: {bulk_costs}")
     
--- day12/day12_test.py
+++ day12/day12_test.py
@@ -5,64 +5,111 @@ def test_example1():
     file_name = "example1"
     data = read_input(file_name)
     plants = traverse_grid(data)
-    costs = calculate_total_cost(plants)
+    costs,bulk_costs = calculate_total_cost(plants)
     assert plants[0]["area"] == 4
-    assert plants[0]["perimiter"] == 10
+    assert plants[0]["perimeter"] == 10
+    assert plants[0]["sides"] == 4
     assert plants[1]["area"] == 4
-    assert plants[1]["perimiter"] == 8
+    assert plants[1]["perimeter"] == 8
+    assert plants[1]["sides"] == 4
     assert plants[2]["area"] == 4
-    assert plants[2]["perimiter"] == 10
+    assert plants[2]["perimeter"] == 10
+    assert plants[2]["sides"] == 8
     assert plants[3]["area"] == 1
-    assert plants[3]["perimiter"] == 4
+    assert plants[3]["perimeter"] == 4
+    assert plants[3]["sides"] == 4
     assert plants[4]["area"] == 3
-    assert plants[4]["perimiter"] == 8
+    assert plants[4]["perimeter"] == 8
+    assert plants[4]["sides"] == 4
     assert len(plants) == 5
     assert costs == 140
+    assert bulk_costs == 80
 
 def test_example2():
     file_name = "example2"
     data = read_input(file_name)
     plants = traverse_grid(data)
-    costs = calculate_total_cost(plants)
+    costs,bulk_costs = calculate_total_cost(plants)
     assert plants[0]["area"] == 21
-    assert plants[0]["perimiter"] == 36
+    assert plants[0]["perimeter"] == 36
     assert plants[1]["area"] == 1
-    assert plants[1]["perimiter"] == 4
+    assert plants[1]["perimeter"] == 4
     assert plants[2]["area"] == 1
-    assert plants[2]["perimiter"] == 4
+    assert plants[2]["perimeter"] == 4
     assert plants[3]["area"] == 1
-    assert plants[3]["perimiter"] == 4
+    assert plants[3]["perimeter"] == 4
     assert plants[4]["area"] == 1
-    assert plants[4]["perimiter"] == 4
+    assert plants[4]["perimeter"] == 4
     assert len(plants) == 5
     assert costs == 772
+    assert bulk_costs == 436
 
 def test_example3():
     file_name = "example3"
     data = read_input(file_name)
     plants = traverse_grid(data)
-    costs = calculate_total_cost(plants)
+    costs, bulk_costs = calculate_total_cost(plants)
     assert plants[0]["area"] == 12
-    assert plants[0]["perimiter"] == 18
+    assert plants[0]["perimeter"] == 18
+    assert plants[0]["sides"] == 10
     assert plants[1]["area"] == 4
-    assert plants[1]["perimiter"] == 8
+    assert plants[1]["perimeter"] == 8
+    assert plants[1]["sides"] == 4
     assert plants[2]["area"] == 14
-    assert plants[2]["perimiter"] == 28
+    assert plants[2]["perimeter"] == 28
+    assert plants[2]["sides"] == 22
     assert plants[3]["area"] == 10
-    assert plants[3]["perimiter"] == 18
+    assert plants[3]["perimeter"] == 18
+    assert plants[3]["sides"] == 12
     assert plants[4]["area"] == 13
-    assert plants[4]["perimiter"] == 20
+    assert plants[4]["perimeter"] == 20
+    assert plants[4]["sides"] == 10
     assert plants[5]["area"] == 11
-    assert plants[5]["perimiter"] == 20
+    assert plants[5]["perimeter"] == 20
+    assert plants[5]["sides"] == 12
     assert plants[6]["area"] == 1
-    assert plants[6]["perimiter"] == 4
+    assert plants[6]["perimeter"] == 4
+    assert plants[6]["sides"] == 4
     assert plants[7]["area"] == 13
-    assert plants[7]["perimiter"] == 18
+    assert plants[7]["perimeter"] == 18
+    assert plants[7]["sides"] == 8
     assert plants[8]["area"] == 14
-    assert plants[8]["perimiter"] == 22
+    assert plants[8]["perimeter"] == 22
+    assert plants[8]["sides"] == 16
     assert plants[9]["area"] == 5
-    assert plants[9]["perimiter"] == 12
+    assert plants[9]["perimeter"] == 12
+    assert plants[9]["sides"] == 6
     assert plants[10]["area"] == 3
-    assert plants[10]["perimiter"] == 8
+    assert plants[10]["perimeter"] == 8
+    assert plants[10]["sides"] == 6
     assert len(plants) == 11
     assert costs == 1930
+    assert bulk_costs == 1206
+
+def test_example4():
+    file_name = "example4"
+    data = read_input(file_name)
+    plants = traverse_grid(data)
+    _, bulk_costs = calculate_total_cost(plants)
+    assert len(plants) == 3
+    assert plants[0]["area"] == 17
+    assert plants[0]["sides"] == 12
+    assert plants[1]["area"] == 4
+    assert plants[1]["sides"] == 4
+    assert plants[2]["area"] == 4
+    assert plants[2]["sides"] == 4
+    assert bulk_costs == 236
+
+def test_example5():
+    file_name = "example5"
+    data = read_input(file_name)
+    plants = traverse_grid(data)
+    _, bulk_costs = calculate_total_cost(plants)
+    assert len(plants) == 3
+    assert plants[0]["area"] == 28
+    assert plants[0]["sides"] == 12
+    assert plants[1]["area"] == 4
+    assert plants[1]["sides"] == 4
+    assert plants[2]["area"] == 4
+    assert plants[2]["sides"] == 4
+    assert bulk_costs == 368
